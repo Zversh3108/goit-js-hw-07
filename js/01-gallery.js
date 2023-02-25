bcfa1a31ae515e48db83fc6c8487cbaa7cf3166b
Zversh3108 <zvershzt3108@gmail.com>
2023-02-25 22:18:46 +0100

    Додав видалення слухача напряму при замиканні модалки

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -25,17 +25,25 @@ gallery.addEventListener("click", onImageClick);
 
 function onImageClick(evt) {
   evt.preventDefault();
-  window.addEventListener("keydown", onCloseModal);
+
   const isImageEl = evt.target.classList.contains("gallery__image");
   if (!isImageEl) {
     return;
   }
   const instance = basicLightbox.create(
-    `<img src="${evt.target.dataset.source}" width="800" height="600">`
+    `<img src="${evt.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", onCloseModal);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", onCloseModal);
+        console.log("sdfsdf");
+      },
+    }
   );
   instance.show();
   function onCloseModal(evt) {
-    window.removeEventListener("keydown", onImageClick);
     if (evt.code === "Escape") {
       instance.close();
     }
